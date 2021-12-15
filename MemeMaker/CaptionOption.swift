8e9b330217e1f1250b94bf8bb03773e3bf041490
Tomiwa Babalola <tbabalola004@gmail.com>
2021-12-15 09:27:55 -0500

    kjdjjldks

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,12 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by Tomiwa Babalola on 12/14/21.
+//
+
+import Foundation
+struct CaptionOption{
+    var emoji:String
+    var caption:String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var topSegmentedControl: UISegmentedControl!
+    @IBOutlet var bottomSegmentedControl: UISegmentedControl!
+    @IBOutlet var topCaptionLabel: UILabel!
+    @IBOutlet var bottomCaptionLabel: UILabel!
+    
+    
+    var topChoices: [CaptionOption] = [CaptionOption(emoji: "🧍‍♂️", caption:"Did we have homework?"), CaptionOption(emoji: "😐", caption:"Lebron is better than Durant."), CaptionOption(emoji: "🙄", caption: "We have to wear blazers tomorrow?")]
+    
+    var bottomChoices:[CaptionOption] = [CaptionOption(emoji:"😑", caption:"Dont know, Dont care."), CaptionOption(emoji: "🤨", caption: "Are you crazy?"), CaptionOption(emoji: "😪", caption: "Yea not me stay safe tho.")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topSegmentedControl.removeAllSegments()
+        
+        bottomSegmentedControl.removeAllSegments()
+        
+        for choice in topChoices {
+            topSegmentedControl.insertSegment(withTitle: choice.emoji,at:topChoices.count,animated: false)
+        }
+        for choice in bottomChoices {
+            bottomSegmentedControl.insertSegment(withTitle: choice.emoji,at:bottomChoices.count,animated: false)
+        }
+        
+        topSegmentedControl.selectedSegmentIndex = 0
+        bottomSegmentedControl.selectedSegmentIndex = 0
+        updateCaptions()
+      
+       
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    func  updateCaptions(){
+        let topIndex = topSegmentedControl.selectedSegmentIndex
+        topCaptionLabel.text = topChoices[topIndex].caption
+        let bottomIndex = bottomSegmentedControl.selectedSegmentIndex
+        bottomCaptionLabel.text = bottomChoices[bottomIndex].caption
+    }
+ 
+    
+    @IBAction func options(_ sender: UISegmentedControl) {
+        updateCaptions()
+       
+    }
+    
 
 }
 
